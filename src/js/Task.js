701b7dbd446ace597d4d4d0f1e2bbb9ce846a236
Kristina <kachalkovakristina@mail.ru>
2023-12-29 19:17:58 +0300

    1.0.1

--- src/js/Task.js
+++
@@ -1,5 +0,0 @@
-export default class Task {
-  constructor(task) {
-    this.task = task;
-  }
-}
--- src/js/filter.js
+++
@@ -1,7 +0,0 @@
-export function filterBy(tasks, callback) {
-  return tasks.filter(callback);
-}
-export function containsText(data, search) {
-  const clean = search.trim().toLowerCase();
-  return data.toLowerCase().includes(clean);
-}
